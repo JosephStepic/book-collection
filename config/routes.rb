ce268d5be3f8d0ea23ce020b6266234c5a98385b
Zach Stepic <stepic.joseph@gmail.com>
2022-01-29 19:48:42 -0600

    fixed layout of site

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :book2s
   resources :books
   resources :book_collections
-  root 'book3s#index'
+  root 'books#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
