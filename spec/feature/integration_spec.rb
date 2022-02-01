ead9c246aeb71edd2e45dc277006a7313b6b7c96
Zach Stepic <stepic.joseph@gmail.com>
2022-01-31 18:35:29 -0600

    introduce integration/unit test

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -7,11 +7,14 @@ RSpec.describe 'Creating a book', type: :feature do
 		fill_in 'Title', with: 'hairry potter'
 		fill_in 'Author', with: 'jk'
 		fill_in 'Price', with: '4'
-		fill_in 'book_published_date_1i', with: '2017'
-		fill_in 'book_published_date_2i', with: 'January'
-		fill_in 'book_published_date_3i', with: '1'
+		select '2021', :from => 'book_published_date_1i'
+		select 'January', :from => 'book_published_date_2i'
+		select '22', :from => 'book_published_date_3i'
 		click_on 'Create Book'
 		visit books_path
-		expect(page).to have_content('hairry potter', 'jk', '4', '2017', 'January', '1')
+		expect(page).to have_content('hairry potter')
+		expect(page).to have_content('jk')
+		expect(page).to have_content('4')
+		expect(page).to have_content('2021-01-22')
 	end
 end
